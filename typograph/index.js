d378be5a486920bb348849ab6900d54f38153034
Mihas <32958803+MrMihas@users.noreply.github.com>
2021-12-19 20:41:08 +0200

    Update index.js

--- typograph/index.js
+++ typograph/index.js
@@ -6,44 +6,87 @@ let copy = document.querySelector(".copy");
 let deleteText = document.querySelector(".deleteText");
 
 
+checkingText.addEventListener('input', ()=>{
+    
+    btn.removeAttribute('disabled');
+    checkingText.classList.remove('access');
+
+});
+
+checkingText.addEventListener('change', ()=>{
+    
+    btn.removeAttribute('disabled');
+    checkingText.classList.remove('access');
+
+});
+
+function startText(){
+    btn.removeAttribute('disabled');
+    
+}
+
 btn.addEventListener("click", () => {
-    const search = " " + "\"";
-    const replaceWith = ' «';
-    let str = " " + checkingText.value ;
-    const result = str.replaceAll(search, replaceWith);
-    next(result);
+    germanLeft();
 });
 
 
+//german
+
+function germanLeft(){
+    const search = "„";
+    const replaceWith = ' «';
+    let str = " " + checkingText.value ;
+    const result = str.replaceAll(search, replaceWith);
+    germanRight(result);
+}
+
+function germanRight(text) {
+    search = "“";
+    replaceWith = '»';
+    result = text.replaceAll(search, replaceWith);
+    englandLeft(result);
+}
+
+//england
+function englandLeft(text){
+    const search = " "+"“";
+    const replaceWith = ' «';
+    let str = " " + text ;
+    const result = str.replaceAll(search, replaceWith);
+    englandRight(result);
+}
+
+function englandRight(text) {
+    search = "”";
+    replaceWith = '»';
+    result = text.replaceAll(search, replaceWith);
+    next(result);
+}
 
 function next(text) {
     search = '\"';
     replaceWith = '»';
     result = text.replaceAll(search, replaceWith);
-    test(result);
-}
-checkingText.addEventListener('focus', ()=>{
-    
-    btn.removeAttribute('disabled');
-
-});
-
-    if(checkingText.value !== ""){
-    btn.removeAttribute('disabled');
-} else{
-    btn.setAttribute('disabled', 'true');
+    reverseSymbol(result);
 }
 
 
-
-function test(text) {
+function reverseSymbol(text) {
     var search = ['\n' + "»"];
     const replaceWith = ['\n' +'«'];
     const result = text.replaceAll(search, replaceWith);
+    reverseSymbol2(result);
+}
+
+function reverseSymbol2(text) {
+    var search = [' ' + "»"];
+    const replaceWith = [' ' +'«'];
+    const result = text.replaceAll(search, replaceWith);
     let checkingText = document.querySelector(".text");
     checkingText.value = result.trim();
-    // checkingText.classList.add('access');
+    checkingText.classList.add('access');
 }
+
 
 copy.addEventListener('click', function () {
     let copyText = checkingText.value;
@@ -62,6 +105,7 @@ navigator.clipboard.writeText(copyText);
 deleteText.addEventListener('click', ()=>{
     checkingText.value = '';
     if(checkingText.value === ''){
+        checkingText.classList.remove('access');
         checkingText.setAttribute('placeholder', 'Введите текст');
     }
 })

1750f0d739d20e1abfd34e64a3df873e5ec6aae4
Mihas <32958803+MrMihas@users.noreply.github.com>
2022-01-05 14:25:04 +0200

    Update app.js

--- Metronome/app.js
+++ Metronome/app.js
@@ -235,7 +235,7 @@ for (const key in localStorage) {
         liSpan.classList.add('planning__tempo');
         li.classList.add('planning__name')
     
-        if(isNaN(element)) {break}
+        if(isNaN(element) || element === null || element === undefined) {break}
 
         liSpan.setAttribute('bit', element);
         planTitle.setAttribute('bit', element )

7cb0c3151b08781383f970c64c6617c3763e0814
Mihas <32958803+MrMihas@users.noreply.github.com>
2022-01-11 20:42:01 +0200

    Add files via upload

---
+++ meTeam/js/common.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	$(function() { // Dropdown toggle
+		$('.dropdown-toggle').click(function() {
+			$(this).toggleClass('accent');
+			$(this).next('.dropdown').slideToggle();
+		});
+		
+		// $(document).click(function(e) 
+		// { 
+		// var target = e.target; 
+		// if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) 
+		// //{ $('.dropdown').hide(); }
+		//   { 
+		// 	  $('.dropdown').slideUp();
+		//   $(this).removeClass('accent');
+		//  }
+
+		// });
+		});
+
+})
